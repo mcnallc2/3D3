919b3cdb208bc042111d6efe897d2abcfd72f175
mcnallc2 <mcnallc2@tcd.ie>
2019-02-26 10:08:17 +0000

    added header file

---
+++ Project1/http.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+class HTTP_request {
+
+public:
+
+	HTTP_request(string name);
+
+};
